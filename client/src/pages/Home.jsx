0b57c3b4b0020a56641ea443bc59fe4eb9d94e3e
Baibhav Sinha <baibhavsinha.dev@gmail.com>
2026-06-04 16:18:26 +0530

    feat: implement hero section with movie details and call-to-action

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Hero from "../components/Hero";
+
 const Home = () => {
     return (
-        <div>
-            Home Component
-        </div>
+        <>
+            <Hero />
+        </>
     );
 };
 
