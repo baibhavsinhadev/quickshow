bf47c147a7d6fc975b013ca29552fcb69256a3ea
Baibhav Sinha <baibhavsinha.dev@gmail.com>
2026-06-05 18:43:19 +0530

    feat: implement admin layout with navbar, sidebar and nested routing structure

--- client/src/components/admin/Sidebar.jsx
+++ client/src/components/admin/Sidebar.jsx
@@ -1,7 +1,58 @@
+import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from "lucide-react";
+import { useAppContext } from "../../context/AppContext";
+import { NavLink } from "react-router-dom";
+
 const Sidebar = () => {
+
+    const { user } = useAppContext();
+
+    const adminNavLinks = [
+        {
+            name: "Dashboard",
+            path: "/admin",
+            icon: LayoutDashboardIcon
+        },
+        {
+            name: "Add Shows",
+            path: "/admin/add-show",
+            icon: PlusSquareIcon
+        },
+        {
+            name: "List Shows",
+            path: "/admin/list-shows",
+            icon: ListIcon
+        },
+        {
+            name: "List Bookings",
+            path: "/admin/list-bookings",
+            icon: ListCollapseIcon
+        },
+    ]
+
     return (
-        <div>
-            Sidebar Component
+        <div className="h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm">
+            <img src={user.imageUrl} alt="imageUrl" className="h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto" />
+
+            <p className="mt-2 text-base max-md:hidden">{user.firstName} {user.lastName}</p>
+
+            <div className="w-full">
+                {adminNavLinks.map((link) => {
+                    const Icon = link.icon
+
+                    return (
+                        <NavLink className={({ isActive }) => `relative flex items-center max-md:justify-center gap-2 w-full py-2.5 md:pl-10 first:mt-6 text-gray-400 ${isActive && "bg-primary/15 text-primary group"}`} end key={link.name} to={link.path}>
+                            {({ isActive }) => (
+                                <>
+                                    <Icon className="w-5 h-5" />
+                                    <p>{link.name}</p>
+
+                                    <span className={`w-1.5 h-10 rounded-l right-0 absolute ${isActive && "bg-primary"}`} />
+                                </>
+                            )}
+                        </NavLink>
+                    );
+                })}
+            </div>
         </div>
     );
 };
--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom"
+import AdminNavbar from "../../components/admin/AdminNavbar";
+import Sidebar from "../../components/admin/Sidebar";
 
 const Layout = () => {
     return (
-        <div>
-            Layout Component
-            {<Outlet />}
-        </div>
+        <>
+            <AdminNavbar />
+
+            <div className="flex">
+                <Sidebar />
+
+                <div className="flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto">
+                    <Outlet />
+                </div>
+            </div>
+        </>
     );
 };
 
