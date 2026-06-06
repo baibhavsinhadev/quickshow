61cad872bc80afdfb2c83faa3b1469987fabeed7
Baibhav Sinha <baibhavsinha.dev@gmail.com>
2026-06-06 11:54:23 +0530

    fix: trim added in full name in functions.js

--- server/inngest/functions.js
+++ server/inngest/functions.js
@@ -6,7 +6,7 @@ const syncUserCreation = inngest.createFunction(
     { id: "sync-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const fullName = first_name + " " + (last_name || "");
+        const fullName = (first_name + " " + (last_name || "")).trim();
 
         const userData = {
             _id: id,
@@ -24,14 +24,14 @@ const syncUserUpdate = inngest.createFunction(
     { id: "update-user-from-clerk", triggers: [{ event: "clerk/user.updated" }] },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const fullName = first_name + " " + (last_name || "");
-        
+        const fullName = (first_name + " " + (last_name || "")).trim();
+
         const updatedData = {
             email: email_addresses?.[0]?.email_address || "",
             name: fullName,
             image: image_url,
         };
-        
+
         await User.findByIdAndUpdate(id, updatedData, { upsert: true });
     }
 );
